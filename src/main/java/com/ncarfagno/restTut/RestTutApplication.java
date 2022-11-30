6827921e7a6b8a40f7f975cffb2dcbbc7486a298
nick <nicholas.a.carfagno@gmail.com>
2022-11-30 16:38:27 -0500

    Initial commit

---
+++ src/main/java/com/ncarfagno/restTut/RestTutApplication.java
@@ -0,0 +1,13 @@
+package com.ncarfagno.restTut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncarfagno/restTut/RestTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncarfagno.restTut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
